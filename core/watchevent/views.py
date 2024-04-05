3683ad389762f5c7f4e99ac7ce15ccf79db76fda
Hassan Ahmadi <h20.ahmadi@gmail.com>
2024-04-05 03:56:43 +0330

    added transasction atomic to ensure consistency

--- core/watchevent/views.py
+++ core/watchevent/views.py
@@ -2,10 +2,12 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .serializers import WatchEventSerializer
+from django.db import transaction
 
 # Create your views here.
 
 @csrf_exempt
+@transaction.atomic
 def event_receiver(request):
     """ recieves json events from external service """
     if request.method == 'POST':
